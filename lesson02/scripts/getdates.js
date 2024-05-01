ae745f1075e467163ace6eb7efe6e8ab5af0f6ab
randy.n.lee@gmail.com <randy.n.lee@gmail.com>
2024-05-01 08:08:55 -0600

    upload new content

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,6 @@
+// lastModified
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Updated: " + text;
+
+// Copyright
+document.getElementById("copyrightYear").innerHTML = new Date().getFullYear();
