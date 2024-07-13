3171a7dda6e3b9aa6e388a8f9609cd776b5a9a9b
randy.n.lee@gmail.com <randy.n.lee@gmail.com>
2024-07-13 16:58:20 -0600

    update

---
+++ scoots/scripts/getDates.js
@@ -0,0 +1,7 @@
+// lastModified
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modification: " + text;
+
+// Copyright
+document.getElementById("copyrightYear").innerHTML = new Date().getFullYear();
+
